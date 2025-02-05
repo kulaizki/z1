d2024e01f83558e2e8cb1c24b7c41613f16fe667
kulaizki <kulaizke@gmail.com>
2025-02-05 22:42:10 +0800

    feat: add margin bottom to markdown p

--- src/lib/components/AdviceCard.svelte
+++ src/lib/components/AdviceCard.svelte
@@ -3,6 +3,13 @@
   export let analysis: string;
 </script>
 
+<style>
+  /* Apply global styling to dynamically generated <p> elements */
+  :global(p) {
+    margin-bottom: 1em; 
+  }
+</style>
+
 <div class="mt-6 rounded-lg bg-slate-800 p-6 shadow-xl text-white max-w-7xl">
   <h2 class="text-2xl font-semibold mb-4 text-orange-500">Analysis</h2>
   <Markdown md={analysis} />
