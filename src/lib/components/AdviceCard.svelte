eba10c68c11ceab909abe0b0e6a04be926b0baed
kulaizki <kulaizke@gmail.com>
2025-02-16 17:37:02 +0800

    refactor: update analysis to player summary

--- src/lib/components/AdviceCard.svelte
+++ src/lib/components/AdviceCard.svelte
@@ -11,6 +11,6 @@
 </style>
 
 <div class="mt-6 rounded-lg bg-slate-800 p-6 shadow-xl text-white max-w-7xl">
-  <h2 class="text-2xl font-semibold mb-4 text-orange-500">Analysis</h2>
+  <h2 class="text-2xl font-semibold mb-4 text-orange-500">Player Summary</h2>
   <Markdown md={analysis} />
 </div>
