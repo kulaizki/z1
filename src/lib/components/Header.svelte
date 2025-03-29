0a9a4d360de5abf5dd40f5fd37c10185b52b08c7
kulaizki <kulaizke@gmail.com>
2025-03-29 16:08:41 +0800

    refactor: remove broke header & footer routes

--- src/lib/components/Header.svelte
+++
@@ -1,7 +0,0 @@
-<header class="w-full py-4 bg-gray-900 border-b border-gray-600 text-white z-10">
-  <div class="max-w-6xl mx-auto px-6 flex flex-col sm:flex-row justify-between items-center">
-    <nav class="flex space-x-4">
-      <a href="/privacy-policy" class="hover:text-orange-500">Privacy Policy</a>
-    </nav>
-  </div>
-</header>
