6d2d635c04e6645d698bb07bf92113cbda26a3a5
kulaizki <kulaizke@gmail.com>
2025-10-20 22:43:22 +0800

    feat: configure Manrope font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,14 @@
 import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ['"Manrope"', ...defaultTheme.fontFamily.sans]
+			},
 			minWidth: {
 				'4xl': '56rem'
 			}
